57ae9c579a096025c587f0cebcd336642f7a7ebc
Gian Carlo <139979093+GCC76@users.noreply.github.com>
2023-07-26 16:09:53 +0200

    Add files via upload

---
+++ package/load_data.js
@@ -0,0 +1,47 @@
+//	*********************************************
+//	formCompiler BY Gian Carlo Ciaccolini
+//	https://github.com/GCC76
+//	Not for commercial use
+//
+
+const form = document.querySelector("#myForm");
+
+const fileReader = (file,callback) => {
+
+	let reader = new FileReader();
+	reader.onload = (event) => {
+		const fileContent = JSON.parse(event.target.result);
+		callback(fileContent);
+	}
+	reader.readAsText(file);
+}
+
+
+form.addEventListener("submit", event => {
+	
+	event.preventDefault();
+	
+	let file = form.file.files[0];
+	let data = fileReader(file, fileData =>{
+		dataSend(fileData);
+	})
+	
+	form.file.value = '';
+	
+})
+
+
+function dataSend(data){
+	
+	chrome.tabs.query({active: true}, async function(tabs){
+		const response = await chrome.tabs.sendMessage(tabs[0].id,data);
+	})
+}
+
+
+
+
+	
+	
+	
+
